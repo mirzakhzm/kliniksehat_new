393836473ca885cf504046ce1f2f21816bcdc4e8
mirzakhzm <mirzakhazim15@gmail.com>
2024-09-27 16:34:48 +0700

    final

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -10,28 +10,10 @@ class DashboardController extends Controller
     {
         // Menampilkan Skor dalam Dashboard
         $totalPasien = Pasien::count();
-        $totalBPJS = Pasien::where('pembayaran', 'BPJS')->count();
-        $totalUmum = Pasien::where('pembayaran', 'Umum')->count();
+        $totalBPJS = Pasien::where('jenis_pasien', 'BPJS')->count();
+        $totalUmum = Pasien::where('jenis_pasien', 'Umum')->count();
         $totalDokter = 25; // Misalnya Anda punya data dokter dari model lain atau hardcode
-
-        // Mengambil data pasien per bulan
-        $bpjsData = Pasien::selectRaw('MONTH(created_at) as month, COUNT(*) as count')
-            ->where('pembayaran', 'Bpjs')
-            ->groupBy('month')
-            ->orderBy('month')
-            ->pluck('count')
-            ->toArray();
-
-        $umumData = Pasien::selectRaw('MONTH(created_at) as month, COUNT(*) as count')
-            ->where('pembayaran', 'Umum')
-            ->groupBy('month')
-            ->orderBy('month')
-            ->pluck('count')
-            ->toArray();
-
-        // Menyediakan data bulan untuk grafik
-        $months = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
         
-        return view('dashboard', compact('totalPasien', 'totalBPJS', 'totalUmum', 'totalDokter', 'bpjsData', 'umumData', 'months'));
+        return view('dashboard', compact('totalPasien', 'totalBPJS', 'totalUmum', 'totalDokter',));
     }
 }
--- app/Models/Pasien.php
+++ app/Models/Pasien.php
@@ -28,16 +28,16 @@ class Pasien extends Model
     ];
 
 
-    public function hitungDiskon()
-    {
-        if ($this->jenis_pasien == 'BPJS' && $this->total_pembayaran >= 500000) {
-            return $this->total_pembayaran * 0.4; // Diskon 60%
-        } elseif ($this->jenis_pasien == 'Umum' && $this->total_pembayaran < 500000) {
-            return $this->total_pembayaran * 0.9; // Diskon 10%
-        } else {
-            return $this->total_pembayaran; // Tidak ada diskon
-        }
-    }
+    // public function hitungDiskon()
+    // {
+    //     if ($this->jenis_pasien == 'BPJS' && $this->total_pembayaran >= 500000) {
+    //         return $this->total_pembayaran * 0.4; // Diskon 60%
+    //     } elseif ($this->jenis_pasien == 'Umum' && $this->total_pembayaran < 500000) {
+    //         return $this->total_pembayaran * 0.9; // Diskon 10%
+    //     } else {
+    //         return $this->total_pembayaran; // Tidak ada diskon
+    //     }
+    // }
     // Mengatur kolom primary key
     // protected $primaryKey = 'id_pasien'; // Menggunakan kolom id_pasien
 
